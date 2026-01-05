1e2b2a7e0ef34665b91a8b9b40fbcec6d919f4f0
Zain Wania <zain@wania.dev>
2026-01-05 10:49:41 -0500

    Initial Commit

---
+++ LinearInbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinearInbox
+//
+//  Created by Zain Wania on 2026-01-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinearInbox/LinearInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearInboxApp.swift
+//  LinearInbox
+//
+//  Created by Zain Wania on 2026-01-05.
+//
+
+import SwiftUI
+
+@main
+struct LinearInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
